bcc5333c16b64594226c8eb5da799b9a54865f32
Bhupesh <gundabhupesh111@gmail.com>
2021-05-17 12:36:04 +0530

    added function area_Circle

--- Week 1/areaOfCircle.py
+++ Week 1/areaOfCircle.py
@@ -1,3 +1,15 @@
+import math
+
+
+def main():
+    area_Circle()
+
 
 def area_Circle():
     radius_circle = int(input("Enter radius of circle:"))
+    area = math.pi * radius_circle * radius_circle
+    print("area of the circle is:", area)
+    return area
+
+
+main()
