211d8f7855589c696c970cb283ad25aa6753bea7
Bhupesh <gundabhupesh111@gmail.com>
2021-05-23 11:05:35 +0530

    added main, dateTime, area, Reverse files

---
+++ Week 2/Reverse.py
@@ -0,0 +1,10 @@
+def reverse(s):
+    str = ""
+    for i in s:
+        str = i + str
+    return str
+
+
+fname = input("enter your first name:")
+lname = input("enter your last name:")
+print(reverse(lname), reverse(fname))
