77f416d5922c4199f83aabf69bed119cba33279b
Bhupesh <gundabhupesh111@gmail.com>
2021-05-23 12:39:05 +0530

    demo

--- Week 1/Demo.py
+++
@@ -1,7 +0,0 @@
-print("Demo Git")
-print("dddd")
-
-print("dddd")
-print("line 5")
-print("line 6")
-print("line 7")
--- Week 2/DateTime.py
+++
@@ -1,4 +0,0 @@
-import datetime
-
-today_date = datetime.datetime.now()
-print(today_date)
