54142890269842e56ced6c89da9dcfde851baf58
Bhupesh <gundabhupesh111@gmail.com>
2021-05-23 13:50:19 +0530

    added DateTime module

--- Week 2/areaOfCircle.py
+++ Week 2/areaOfCircle.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import math
 
 
@@ -7,13 +6,3 @@ def area_Circle():
     area = math.pi * radius_circle * radius_circle
     print("area of the circle is:", area)
     return area
-=======
-import math
-
-
-def area_Circle():
-    radius_circle = int(input("Enter radius of circle:"))
-    area = math.pi * radius_circle * radius_circle
-    print("area of the circle is:", area)
-    return area
->>>>>>> origin/master
--- Week 2/main.py
+++ Week 2/main.py
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import areaOfCircle
-import Reverse
-
-if __name__ == '__main__':
-    areaOfCircle.area_Circle()
-    Reverse.reverse()
-
-
-=======
 import areaOfCircle
 import DateTime
 
 if __name__ == '__main__':
     areaOfCircle.area_Circle()
-    
->>>>>>> origin/master
+    DateTime.today_date
+
+
+
