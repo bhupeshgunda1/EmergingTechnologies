d8f340dd748b26c4a0b5bc58c2ca9f3852b143bf
BhupeshGunda <35622869+bhupeshgunda1@users.noreply.github.com>
2021-05-23 12:16:24 +0530

    Add files via upload

---
+++ Week 2/areaOfCircle.py
@@ -0,0 +1,8 @@
+import math
+
+
+def area_Circle():
+    radius_circle = int(input("Enter radius of circle:"))
+    area = math.pi * radius_circle * radius_circle
+    print("area of the circle is:", area)
+    return area
---
+++ Week 2/main.py
@@ -0,0 +1,5 @@
+import areaOfCircle
+
+if __name__ == '__main__':
+    areaOfCircle.area_Circle()
+    
